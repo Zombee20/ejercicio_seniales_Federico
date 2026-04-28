d5e70d4842d41ef69954e59d440343d6fb1650d1
Federico Fernández <federico.fernandez.augusto@gmail.com>
2026-04-28 19:25:22 -0300

    Actualización de contador de instancias
    comienza a hacerse uso de la función "count_new_instance" cada vez que se presiona el bloque rojo o violeta

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -5,6 +5,10 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 var instancesCount = 0
 
 func _ready():
+	var BlockRed = get_node("RedGenerator")
+	var BlockViolet = get_node("VioletGenerator")
+	BlockRed.button_down.connect(count_new_instance)
+	BlockViolet.button_down.connect(count_new_instance)
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
